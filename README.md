e7c008a91882ed6417773bdca9356d56070b85a0
Chikage <kazuto@atx.tokyo>
2020-02-06 02:01:20 +0900

    adminLTE追加

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 docker-compose up -d --build  
 docker exec -it php-docker-test_php_1 /var/www/html/test/init.sh  
+
+    
+docker exec -it php-docker-test_php_1 composer update
---
+++ src/test/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('adminlte::page')
+ 
+<!-- タイトル -->
+@section('title', 'Dashboard')
+ 
+<!-- 見出し -->
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+ 
+<!-- 内容 -->
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
