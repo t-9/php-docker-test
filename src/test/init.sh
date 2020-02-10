94a3a09ca2314d7bf54a9903dc8d2356a87df642
Chikage <kazuto@atx.tokyo>
2020-02-11 00:28:12 +0900

    db初期設定用処理追加

--- src/test/init.sh
+++ src/test/init.sh
@@ -3,6 +3,13 @@
 cd /var/www/html/test
 composer install
 cp .env.example .env
+
+sed -e 's/DB_HOST=127\.0\.0\.1/DB_HOST=db/g' .env > .env.tmp1
+sed -e 's/DB_DATABASE=laravel/DB_DATABASE=docker/g' .env.tmp1 > .env
+sed -e 's/DB_USERNAME=root/DB_USERNAME=user/g' .env > .env.tmp1
+sed -e 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env.tmp1 > .env
+rm .env.tmp1
+
 php artisan key:generate
 php artisan config:clear
 
