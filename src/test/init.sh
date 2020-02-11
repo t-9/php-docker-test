8a0ab4d5619c69dde3f308aae025500738240b82
Chikage <kazuto@atx.tokyo>
2020-02-11 19:15:31 +0900

    redisの追加に伴い.envを変更

--- src/test/init.sh
+++ src/test/init.sh
@@ -10,6 +10,12 @@ sed -e 's/DB_USERNAME=root/DB_USERNAME=user/g' .env > .env.tmp1
 sed -e 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env.tmp1 > .env
 rm .env.tmp1
 
+sed -e 's/REDIS_HOST=127\.0\.0\.1/REDIS_HOST=redis/g' .env > .env.tmp1
+sed -e 's/CACHE_DRIVER=file/CACHE_DRIVER=redis/g' .env.tmp1 > .env
+sed -e 's/SESSION_DRIVER=file/SESSION_DRIVER=redis/g' .env > .env.tmp1
+cp .env.tmp1 .env
+rm .env.tmp1
+
 php artisan key:generate
 php artisan config:clear
 
