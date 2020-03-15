690b88198fdd9e30ca7d01f53a4b38f786b785f3
Chikage <t-9@atx.tokyo>
2020-03-15 23:54:56 +0900

    propsの型を指定する

--- src/test/resources/js/components/vue-select.js
+++ src/test/resources/js/components/vue-select.js
@@ -9,7 +9,24 @@ Vue.component('v-sel', {
       selected: null
     }
   },
-  props: ['options', 'value', 'name', 'required'],
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    value: {
+      type: String,
+      required: false
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    required: {
+      type: Boolean,
+      required: false
+    }
+  },
   template: `<v-select v-bind:options="options" v-model="selected" v-on="setInitialValue(value)" :reduce="label => label.value" label="label">
   <template #search="{attributes, events}">
   <input :name="name" class="vs__search" :required="required == true" v-bind="attributes" v-on="events" v-model="selected" />
